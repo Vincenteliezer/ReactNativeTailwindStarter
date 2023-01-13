8e038bd43c1f96079b775745fc3bfdb085bc62eb
Vincent Eliezer <eliezervincent97@gmail.com>
2023-01-13 05:02:06 +0300

    project init

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, } from 'react-native'
 import React from 'react'
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text className="text-black">HomeScreen</Text>
+        {/* <StatusBar barStyle={'light-content'} /> */}
+      <Text className="text-black text-2xl">HomeScreen</Text>
     </View>
   )
 }
